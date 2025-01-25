cc72a40bf80f05412eecda6c6a6a4eae00bbcc47
sepehr-safaeian <sepehrsafaeian@gmail.com>
2025-01-25 08:17:46 +0330

    Performance edit/ remove a start function

--- cmd/start.go
+++ cmd/start.go
@@ -2,18 +2,31 @@ package cmd
 
 import (
 	"PomodoroCLI/internal"
+	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 func startPomodoro(cmd *cobra.Command, args []string) {
-	internal.StartTimer()
+	duration := 25 * time.Minute
+	if len(args) > 0 {
+		var err error
+		duration, err = time.ParseDuration(args[0])
+		if err != nil {
+			fmt.Println("Invalid duration format. Using default 25 minutes.")
+			duration = 25 * time.Minute
+		}
+	}
+
+	internal.Timer(duration, "Pomodoro Timer Started!")
 }
 
 func init() {
 	rootCmd.AddCommand(&cobra.Command{
-		Use:   "start",
-		Short: "Start a 25-minute Pomodoro timer",
+		Use:   "start [duration]",
+		Short: "Start a Pomodoro timer. Default is 25 minutes.",
+		Args:  cobra.MaximumNArgs(1),
 		Run:   startPomodoro,
 	})
 }
--- internal/timer.go
+++ internal/timer.go
@@ -7,22 +7,30 @@ import (
 	"github.com/briandowns/spinner"
 )
 
-func StartTimer() {
-	duration := 25 * time.Minute
-	fmt.Println("Pomodoro Timer Started!")
+const (
+	WorkDuration  = 25 * time.Minute
+	BreakDuration = 5 * time.Minute
+)
+
+func Timer(duration time.Duration, message string) {
+	fmt.Println(message)
 
 	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 	s.Start()
 
 	startTime := time.Now()
-	for remaining := duration; remaining > 0; remaining -= time.Second {
-		elapsed := time.Since(startTime)
-		remaining = duration - elapsed
-
+	for remaining := duration - time.Since(startTime); remaining > 0; remaining = duration - time.Since(startTime) {
 		s.Suffix = fmt.Sprintf(" Time Remaining: %02d:%02d", int(remaining.Minutes()), int(remaining.Seconds())%60)
 		time.Sleep(1 * time.Second)
 	}
 
 	s.Stop()
-	fmt.Println("\nTime's up! Take a break.")
+	fmt.Println("\nTime's up!")
+}
+
+func PomodoroCycle() {
+	for {
+		Timer(WorkDuration, "Pomodoro Timer Started!")
+		Timer(BreakDuration, "Break Time Started!")
+	}
 }
